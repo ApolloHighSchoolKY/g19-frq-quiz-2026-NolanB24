af98100be0c9399a209fb991c1b42c355504617d
NolanB24 <nolan.bretz@stu.daviess.kyschools.us>
2026-02-09 20:31:17 +0000

    commit

--- Quiz.java
+++ Quiz.java
@@ -1,10 +1,27 @@
 public static String scrambleWord(String word){
   /* To be implemented in part (a) */
-
+  for(int i = 0; i < word.legnth(); i++)
+    word = "";
+    word += word.substring(i + 1, i + 1);
+    
+  return word;
 }
 
 
 public static void scrambleOrRemove(List<String> wordList){
   /* To be implemented in part (b) */
+  for(String x: wordList)
+    if(!x == word.scrambleWord())
+      wordList.remove(x);
+    
+
+  
+
+
+
+
+
+
+
 
 }
